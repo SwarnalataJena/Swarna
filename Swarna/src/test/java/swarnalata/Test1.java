bccb1a0d5db885a9dde3cbec473381c1c0a9fc3e
USER <USER@DESKTOP-FQHLV6B>
2022-06-25 20:50:15 +0530

    my first project for Github

---
+++ Swarna/src/test/java/swarnalata/Test1.java
@@ -0,0 +1,14 @@
+package swarnalata;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void test1()
+	{
+	Reporter.log("good morning", true);	
+	}
+
+}
---
+++ Swarna/src/test/java/swarnalata/Test2.java
@@ -0,0 +1,14 @@
+package swarnalata;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void test2()
+	{
+		Reporter.log("Good night", true);
+	}
+
+}
